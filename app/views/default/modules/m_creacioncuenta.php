9affd45615c19fac6f823de3b2949ff90b9417fc
Franpastoragusti <franciscopa91@gmail.com>
2016-05-02 22:30:33 +0200

    subida de html y rm de tabladatos

--- app/views/default/modules/m_creacioncuenta.php
+++ app/views/default/modules/m_creacioncuenta.php
@@ -8,8 +8,8 @@
 <body>
 	<div class="container">
 		<h1>Formulario de registro</h1>
-			<form method="POST" action="">
-			    <div class="lateral-izquierda">
+		<form method="POST" action="">
+			   <div class="lateral-izquierda">
 					<fieldset>
       					<legend>Datos Usuario</legend>
 				        	<label>Usuario</label> 
@@ -19,29 +19,22 @@
 				        	<label>Confirmar Contraseña:</label> 
 				        	<input type="password" name="contraseña" size="20" maxlengh="20" required/>
 					       	<label class="radio">Restaurante
-					       	<input type="radio" name="tipoUsuario" value="Restaurante">
+					       	<input type="radio" name="tipoUsuario" value="Restaurante" class="radioBut">
 					       	</label>
 				        	<label class="radio">Proveedor
-				        	<input type="radio" name="tipoUsuario" value="Proveedor">
+				        	<input type="radio" name="tipoUsuario" value="Proveedor" class="radioBut">
 				        	</label>
 				    </fieldset>
-				    <input type="submit" value="enviar"></input>
+				    <input type="submit" value="enviar">
 			    </div>
 				<div class="lateral-derecha">
 				    <fieldset>
-				       <legend> Datos Empresa:</legend> 
-				          	<div>
-					          	<label>Nombre Propietario
-					          		<input type="text" id="nombreProp" required/>
-					          	</label>
-					          	<label>Apellidos
-					          		<input type="text" id="apellidos" required/>
-					          	</label>
+				       <legend> Datos Empresa</legend>
 					          	<label>Nombre Empresa
-					          		<input type="text" id="nombreEmp" required>
+					          		<input type="text" id="nombreEmp" required/>
 					          	</label>
 					          	<label>CIF:
-					          		<input type="number" id="cif" required />
+					          		<input type="text" required />
 					          	</label>
 					          	<label>Telefono
 					          		<input type="number" id="telefono" required/>
@@ -49,9 +42,8 @@
 					          	<label>Email:
 					          		<input type="email" id="email" required/>
 					          	</label>
-					        </div>
 						<fieldset>
-				        	<legend>Direccion</legend>
+						<legend> Datos Empresa:</legend>
 				        	<label>Provincia:
 					      		<select name="Provincia">
 							        <option selected="selected">Valencia</option>
@@ -66,17 +58,18 @@
 				        		<input type="text" id="CP" required/>
 				        	</label> 
 				        	<label>Calle
-				        		<input type="text" id="calle" required/>
+				        		<input type="text" required/>
 				        	</label>
 				        	<label>Nº
 				        		<input type="number" id="numero" required/>
-				        	</label> 
-				    	</fieldset>
-				    	<label>Descripción</label>
-					    	<textarea rows="10" cols="90"></textarea>    
+				        	</label>
+				    	<label >Descripción
+					    	<textarea rows="10" cols="50"></textarea>  
+					    </label>  
 				    </fieldset>
-				</div>
-			</form>
+				</fieldset>
+			</div>
+		</form>
 	</div>
 </body>
 </html>
--- app/views/default/modules/m_mostrarRestProv.php
+++ app/views/default/modules/m_mostrarRestProv.php
@@ -6,25 +6,44 @@
 	<link rel="stylesheet" type="text/css" href="../css/f_CreacionCuenta.css">
 	<meta charset="UTF-8">	
 </head>
-	<body id="fondo">
-			<form>
-			    <fieldset>
-			       <legend> Nombre Restaurante</legend> 
-			            <label>Email: <br><input type="email" id="email" required/></label>
-			          	<label>Telefono
-					          		<input type="number" id="telefono" required/>
-					          	</label>
-						<fieldset>
+<title>Proveedor | Tabla Datos Restaurante</title>
+	<body class="formEmergente">
+		<form>
+			<fieldset>
+			    <legend> Nombre Restaurante</legend> 
+			       <label class="inlineBlock">Email: <input type="email" id="email" required/></label>
+			       <label class="inlineBlock">Telefono <input type="number" id="telefono" required/></label>
+				<fieldset>
 							
 			        <legend> Dirección</legend>
-			        	<select name="Provincia">
-							        <option selected="selected">Valencia</option>
-							        <option>Castellón</option>
-							        <option>Alicante</option>
-					      		</select>
-					      </fieldset>
-			    </fieldset>     
-			          <label><input type="submit" value="Enviar"/>
+			        <div class="lateral-izquierda">
+			        	<label for="Provincia">Provincia</label>
+				        <select name="Provincia">
+							<option selected="selected">Valencia</option>
+							<option>Castellón</option>
+							<option>Alicante</option>
+						</select>
+						<label>Localidad 
+						   	<input type="text" id="localidad" required/>
+						</label>
+					</div>
+					<div class="lateral-derecha">
+	      				<label id="cp">CP: 
+	      					<input type="text" id="codigopostal" pattern="[0-9]{5}" required/>
+	      				</label>
+						<label>Calle 
+							<input type="text" id="c" required/>
+						</label>
+						<label>Nº: 
+							<input type="number" id="numeropuerta" />
+						</label>
+					</div>
+				</fieldset>
+			</fieldset> 
+			<div>   
+			    <input class="boton" type="submit" value="Enviar"/>
+			    <input type="button" value="Cancelar" class="boton" onclick= "self.location.href = '../page.php'" />
+			</div> 
 			</form>
 	</body>
 </html>
