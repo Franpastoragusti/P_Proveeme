9d30f52832615964839b8c1e48125d6d78111e25
Franpastoragusti <franciscopa91@gmail.com>
2016-05-03 11:51:29 +0200

    Cambio

--- app/views/default/modules/m_botonesProveedorMP.php
+++ app/views/default/modules/m_botonesProveedorMP.php
@@ -1,5 +1,5 @@
 <ul>
-	<li><a href="indexP.php?action=#">Añadir Producto</a></li>
+	<li><a href="indexP.php?action=hasdhwd">Añadir Producto</a></li>
 	<li><a href="indexP.php?action=#"> Modificar Producto</a></li>
 	<li><a href="indexP.php?action=#">Quitar Producto</a></li>
 </ul>
